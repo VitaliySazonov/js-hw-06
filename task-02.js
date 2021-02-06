3bd01bd481428f7abb7da5118110e7669958c5e7
Vito <topvetal@gmail.com>
2021-02-06 23:47:19 +0200

    task-02

---
+++ task-02.js
@@ -0,0 +1,8 @@
+// Задание 2
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+import users from './users.js'
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color)
+
+console.log(getUsersWithEyeColor(users, 'blue'));
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
